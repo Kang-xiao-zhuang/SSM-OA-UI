291ce1fc9241de8ebd109d4ede5d72b2af482194
Kang-xiao-zhuang <2247830091@qq.com>
2023-03-12 23:40:37 +0800

    提交代码

--- src/views/system/sysRole/list.vue
+++ src/views/system/sysRole/list.vue
@@ -25,7 +25,8 @@
     </div>
     <!-- 工具条 -->
     <div class="tools-div">
-      <el-button type="success" icon="el-icon-plus" size="mini" @click="add">添 加</el-button>
+      <el-button type="success" icon="el-icon-plus" size="mini" :disabled="$hasBP('bnt.sysRole.add')  === false" @click="add">添 加</el-button>
+<!--      <el-button type="success" icon="el-icon-plus" size="mini" @click="add">添 加</el-button>-->
       <el-button class="btn-add" size="mini" @click="batchRemove()">批量删除</el-button>
     </div>
 
